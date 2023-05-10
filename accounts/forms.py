944e4abf30916a81335f2ea1cf8ee7ca1b16d07f
thebjko <thebjko@gmail.com>
2023-05-10 11:07:38 +0900

    [hotfix] import ReadOnlyPasswordHashField

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 from django.utils.translation import gettext_lazy as _
 
 
