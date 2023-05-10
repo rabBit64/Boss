d08de0eb6cdcc4de6bc881e45e3388b2633829c6
thebjko <thebjko@gmail.com>
2023-05-10 21:34:46 +0900

    set users in DATABASES

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,3 +8,15 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True   # False for prod.py
 ALLOWED_HOSTS = []
+
+# mysql database
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-rds.html
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES['users'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('RDS_DB_NAME'),
+        'USER': os.getenv('RDS_USERNAME'),
+        'PASSWORD': os.getenv('RDS_PASSWORD'),
+        'HOST': os.getenv('RDS_HOSTNAME'),
+        'PORT': os.getenv('RDS_PORT'),
+    }
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,3 +8,14 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False   # False for prod.py
 ALLOWED_HOSTS = []
+
+
+# 아직 사용하지 않지만, AWS Secrets Manager에서 가져오는 코드 작성하기
+DATABASES['users'] = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': os.getenv('RDS_DB_NAME'),
+    'USER': os.getenv('RDS_USERNAME'),
+    'PASSWORD': os.getenv('RDS_PASSWORD'),
+    'HOST': os.getenv('RDS_HOSTNAME'),
+    'PORT': os.getenv('RDS_PORT'),
+}
