f002a45aee043e14d71434d0d1776cd82c75c7ae
pty9714 <pty9714@naver.com>
2023-05-10 11:31:56 +0900

    boss/create 임시생성

--- boss/forms.py
+++ boss/forms.py
@@ -5,7 +5,8 @@ from .models import Product, Review
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('name','weight','country',)
+        fields = ('name','weight','country','quantity','image',)
+        
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
