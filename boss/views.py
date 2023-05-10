ef558b5537d8d748089e4bb7cd08898892c28b79
rabBit64 <gojh47@gmail.com>
2023-05-10 10:17:29 +0900

    initial navbar

--- boss/views.py
+++ boss/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+def init(request):
+    return redirect('articles:index')
+
+def index(request):
+    return render(request,'boss/index.html')
