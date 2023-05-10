07e24fa1a585086c0cfc3eb94b774017ccc1a4ba
pty9714 <pty9714@naver.com>
2023-05-10 10:53:29 +0900

    fix_forms

--- boss/forms.py
+++ boss/forms.py
@@ -5,7 +5,7 @@ from .models import Product, Review
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-
+        fields = ('name','weight','country',)
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
