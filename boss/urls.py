f7692c6052603ac6a8e3c635ccb9a9cdebf3b312
pty9714 <pty9714@naver.com>
2023-05-12 06:56:22 +0900

    create_form fix_subcategory

--- boss/urls.py
+++ boss/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('<int:product_pk>/reviews/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
     path('<int:product_pk>/reviews/<int:review_pk>/update/', views.review_update, name='review_update'),
     path('search/', views.search, name='search'),
+    path('subcategory-options/', views.subcategory_options, name='subcategory_options'),
+    
 ]
