40a6a638be3218d8272f639a10532089863711d0
rabBit64 <gojh47@gmail.com>
2023-05-11 11:13:14 +0900

    subcategory hide complete

--- static/main/main.js
+++ static/main/main.js
@@ -3,9 +3,13 @@ let subNum = 0
 //카테고리 메뉴 mouseover시 서브카테고리 박스 보이게
 const categoryButton1 = document.getElementById("sub1") //가공식품
 const categoryButton2 = document.getElementById("sub2") //농수축산물
+const categoryButton3 = document.getElementById("sub3") //배달용품
+const categoryButton4 = document.getElementById("sub4") //주방용품
+
 const subcategoryBox1 = document.getElementById("sub1-2") //가공식품 하위
 const subcategoryBox2 = document.getElementById("sub2-2") //농수축산물 하위
-
+const subcategoryBox3 = document.getElementById("sub3-2") //배달용품 하위
+const subcategoryBox4 = document.getElementById("sub4-2")
 
 categoryButton1.addEventListener("mouseover", (event) => {
   subcategoryBox1.classList.remove("sub-category2-hide")
@@ -18,9 +22,6 @@ categoryButton1.addEventListener("mouseover", (event) => {
       subcategoryBox2.classList.add("sub-category2-hide")
     }
   }
-
-
-
   isSubcategory=true
   console.log(isSubcategory)
   subNum=1
@@ -35,13 +36,39 @@ categoryButton2.addEventListener("mouseover", (event) =>{
     if(subNum==1){
       console.log("실행됨2")
       subcategoryBox1.classList.add("sub-category2-hide")
+    }else if(subNum==3){
+      subcategoryBox3.classList.add("sub-category2-hide")
     }
   }
+  isSubcategory=true
   subNum=2
 })
 
+categoryButton3.addEventListener("mouseover", (event) =>{
+  subcategoryBox3.classList.remove("sub-category2-hide")
+  subcategoryBox3.classList.add("sub-category2-display")
+  if(isSubcategory==true){
+    if(subNum==2){
+      subcategoryBox2.classList.add("sub-category2-hide")
+    }else if(subNum==4){
+      subcategoryBox4.classList.add("sub-category2-hide")
+    }
+  }
+  isSubcategory=true
+  subNum=3
+})
 
-
+categoryButton4.addEventListener("mouseover", (event) =>{
+  subcategoryBox4.classList.remove("sub-category2-hide")
+  subcategoryBox4.classList.add("sub-category2-display")
+  if(isSubcategory==true){
+     if(subNum==3){
+      subcategoryBox3.classList.add("sub-category2-hide")
+     }
+  }
+  isSubcategory=true
+  subNum=4
+})
 
 
 
@@ -54,7 +81,14 @@ subcategoryBox1.addEventListener("mouseleave", (event) =>{
 })
 subcategoryBox2.addEventListener("mouseleave", (event) => {
   subcategoryBox2.classList.add("sub-category2-hide")
+  isSubcategory=false
 })
-
-
+subcategoryBox3.addEventListener("mouseleave", (event)=>{
+  subcategoryBox3.classList.add("sub-category2-hide")
+  isSubcategory=false
+})
+subcategoryBox4.addEventListener("mouseleave", (event)=>{
+  subcategoryBox4.classList.add("sub-category2-hide")
+  isSubcategory=false
+})
 
