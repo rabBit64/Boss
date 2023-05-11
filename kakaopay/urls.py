69c42b6ca1b9ba85f151340557bc3fc492fdfd3d
iblug <1115jkb@gmail.com>
2023-05-11 16:31:08 +0900

    add templates, kakao_ak, install requests

--- kakaopay/urls.py
+++ kakaopay/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'kakaopay'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('kakaopay/', views.kakaopay, name='kakaopay'), # 카트아이디
+    path('pay_success/', views.pay_success, name='pay_success'), # 카트아이디
+    path('pay_fail/', views.pay_fail, name='pay_fail'),
+    path('pay_cancel/', views.pay_cancel, name='pay_cancel'),
 ]
