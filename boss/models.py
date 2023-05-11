88b742a503f2e3ecd016b74e472c245275e5bea7
thebjko <thebjko@gmail.com>
2023-05-11 21:59:43 +0900

    add events field to Product

--- boss/models.py
+++ boss/models.py
@@ -19,20 +19,27 @@ class Subcategory(models.Model):
 
 
 class Product(models.Model):
+    def get_upload_path(instance):
+        return f'products/{instance.user.username}/{instance.name}'
     name = models.CharField(max_length=100)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_products', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, blank=True, null=True)
-    image = models.ImageField(upload_to='product/', blank=True)
+    image = models.ImageField(upload_to=get_upload_path, blank=True)
     price = models.IntegerField(validators=[MinValueValidator(1)])  #상품가격
     weight = models.IntegerField(validators=[MinValueValidator(1)])  # 중량
     quantity = models.IntegerField(default=1) # 수량
     country = models.CharField(max_length=50) # 제조국
+    
     #### 모델 할인율, 1+1 상품 여부, 무료배송 여부 추가
     discount_rate = models.IntegerField(default=0)
+
     one_plus_one = models.BooleanField(default=False)
     delivery_fee = models.IntegerField(default=0)
+    
+    # 쿠폰팩, 기획전을 CharField로 기록
+    events = models.CharField('이벤트', max_length=50, blank=True, default='')
 
     def __str__(self):
         return self.name
