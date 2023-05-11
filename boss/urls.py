d6856f99580cfbd54894e6e7ddc40bf35e0017a0
pty9714 <pty9714@naver.com>
2023-05-11 09:56:10 +0900

    Fix_urls

--- boss/urls.py
+++ boss/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
     path('<int:product_pk>/reviews/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
     path('<int:product_pk>/reviews/<int:review_pk>/update/', views.review_update, name='review_update'),
     path('search/', views.search, name='search'),
+]
