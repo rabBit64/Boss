c487faf2ae3663987ebb8e34a18b4ceb9dfdbc7c
iblug <1115jkb@gmail.com>
2023-05-13 04:19:29 +0900

    Add context_processors

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -4,38 +4,14 @@ from .views import _cart_id
 
 def counter(request):
     item_count = 0
-    # try:
-    #     cart = Cart.objects.get(cart_id=_cart_id(request))
-    # except Cart.DoesNotExist:
-    #     cart = Cart.objects.create(cart_id = _cart_id(request))
-    #     cart.save()
-    # if 'admin' in request.path:
-    #     return {}
-    # else:
     try:
         cart = Cart.objects.filter(cart_id=_cart_id(request))
-        print('@@@@@@@@@@@@@@@@@@@@@@@@', cart)
-        # print(Cart.objects.all())
         cart_items = CartItem.objects.all().filter(cart=cart[:1])
-        # print(len(cart_items))
         item_count = len(cart_items)
-        # print(len(cart.cartitem_set.all()))
-        # cart_items = Cart.objects.all().filter(cart=cart[:1])
-        # for cart_item in cart_items:
-        #     item_count += cart_item.quantity
     except Cart.DoesNotExist:
         item_count = 0
     
     context = {
         'item_count': item_count,
-    }
-    return context
-    # return
-
-from datetime import datetime
-
-def current_datetime(request):
-    context = {
-        'current_datetime': datetime.now(),
     }
     return context
--- config/settings/base.py
+++ config/settings/base.py
@@ -61,7 +61,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.counter',
-                'cart.context_processors.current_datetime',
             ],
         },
     },
