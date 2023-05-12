6011e525b42682790af8573b178687d4dc534735
iblug <1115jkb@gmail.com>
2023-05-13 03:35:32 +0900

    refact cart

--- cart/urls.py
+++ cart/urls.py
@@ -7,6 +7,7 @@ app_name = 'cart'
 urlpatterns = [
     path('add/<int:product_pk>/', views.add_cart, name='add_cart'),
     path('', views.cart_detail, name='cart_detail'),
-    path('remove/<int:product_pk>/', views.cart_remove, name='cart_remove'),
+    path('increase_item/<int:product_pk>/', views.increase_item, name='increase_item'),
+    path('decrease_item/<int:product_pk>/', views.decrease_item, name='decrease_item'),
     path('full_remove/<int:product_pk>/', views.full_remove, name='full_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -12,20 +12,17 @@ def _cart_id(request):
     return cart
 
 
-# 카트 추가 / 상품 개수 추가
+# 카트 추가
 def add_cart(request, product_pk):
     product = Product.objects.get(pk=product_pk)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request),
-        )
+        cart = Cart.objects.create(cart_id = _cart_id(request))
         cart.save()
-    
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
+        cart_item.quantity = 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
@@ -34,13 +31,17 @@ def add_cart(request, product_pk):
             cart = cart
         )
         cart_item.save()
-    return redirect('cart:cart_detail')
+    return redirect('boss:detail', product.pk)
 
 
 # 카트 상세
 def cart_detail(request, total=0, counter=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id = _cart_id(request))
+        cart.save()
+    try:
         cart_items = CartItem.objects.filter(cart=cart, active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
@@ -58,8 +59,26 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
     return render(request, 'cart/cart.html', context)
 
 
+# 상품 수량 증가
+def increase_item(request, product_pk): 
+    product = Product.objects.get(pk=product_pk)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart
+        )
+        cart_item.save()
+    return redirect('cart:cart_detail')
+
+
 # 상품 수량 감소
-def cart_remove(request, product_pk):
+def decrease_item(request, product_pk):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, pk=product_pk)
     cart_item = CartItem.objects.get(product=product, cart=cart)
