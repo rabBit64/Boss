28f1cedc2dd1f3714937c4de7ada7bda8b15ce78
rabBit64 <gojh47@gmail.com>
2023-05-12 10:36:07 +0900

    change upload path default

--- boss/models.py
+++ boss/models.py
@@ -26,7 +26,8 @@ class Product(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_products', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, blank=True, null=True)
-    image = models.ImageField(upload_to=get_upload_path, blank=True)
+    # image = models.ImageField(upload_to=get_upload_path, blank=True)
+    image = models.ImageField(upload_to='product/', blank=True)
     price = models.IntegerField(validators=[MinValueValidator(1)])  #상품가격
     weight = models.IntegerField(validators=[MinValueValidator(1)])  # 중량
     quantity = models.IntegerField(default=1) # 수량
