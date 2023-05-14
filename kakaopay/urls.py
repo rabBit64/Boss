27736ecb55416a98aa23a71825b11ef5bf128359
iblug <1115jkb@gmail.com>
2023-05-15 05:12:27 +0900

    product pay

--- kakaopay/urls.py
+++ kakaopay/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 app_name = 'kakaopay'
 urlpatterns = [
+    path('wait/', views.wait, name='wait'),
+    path('pay/<str:cart_id>/', views.pay, name='pay'),
     path('approval/<str:cart_id>/', views.approval, name='approval'),
     path('pay_fail/', views.pay_fail, name='pay_fail'),
     path('pay_cancel/', views.pay_cancel, name='pay_cancel'),
-    path('wait/', views.wait, name='wait'),
-    path('<str:cart_id>/', views.pay, name='pay'), # 제일 아래에 위치
+
+    path('wait_product/', views.wait_product, name='wait_product'),
+    path('pay_product/<int:product_pk>/<int:count>/', views.pay_product, name='pay_product'),
+    path('approval_product/<int:product_pk>/<int:count>/', views.approval_product, name='approval_product'),
+    path('pay_product_fail/', views.pay_product_fail, name='pay_product_fail'),
+    path('pay_product_cancel/', views.pay_product_cancel, name='pay_product_cancel'),
 ]
